289253afe2fc7ac05d15dba1be2aac19ce7911bf
morishin <sntr92@gmail.com>
2021-04-05 22:56:59 +0900

    Protect updateUser

--- app/users/mutations/updateUser.ts
+++ app/users/mutations/updateUser.ts
@@ -1,19 +1,19 @@
-import { resolver } from "blitz"
+import { AuthorizationError, resolver } from "blitz"
 import db from "db"
 import * as z from "zod"
 
-const UpdateUser = z
-  .object({
-    id: z.number(),
-    name: z.string(),
-  })
-  .nonstrict()
+const UpdateUser = z.object({
+  id: z.number(),
+  name: z.string(),
+})
 
 export default resolver.pipe(
   resolver.zod(UpdateUser),
   resolver.authorize(),
-  async ({ id, ...data }) => {
-    // TODO: in multi-tenant app, you must add validation to ensure correct tenant
+  async ({ id, ...data }, { session }) => {
+    if (session.userId !== id) {
+      throw new AuthorizationError()
+    }
     const user = await db.user.update({ where: { id }, data })
 
     return user
