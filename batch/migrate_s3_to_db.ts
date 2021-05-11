e33d3169bf0ccad7d11bce2d2ba8c22c65580a17
Kazuhito Hokamura <k.hokamura@gmail.com>
2021-05-11 22:19:58 +0900

    Update s3 data migration batch

--- batch/migrate_s3_to_db.ts
+++ batch/migrate_s3_to_db.ts
@@ -1,13 +1,15 @@
 import db from "../db"
 import { downloadResultTSV } from "../app/core/lib/s3"
+import { convertTsvToQueryResult } from "../app/core/lib/QueryResult"
 ;(async () => {
   const queries = await db.bdashQuery.findMany({ select: { id: true, id_hash: true } })
   for (const query of queries) {
     const data = await downloadResultTSV(query.id_hash)
     const tsv = data.Body?.toString()
     if (!tsv) continue
+    const result = convertTsvToQueryResult(tsv)
     await db.bdashQuery.update({
-      data: { result: tsv },
+      data: { result: result ? JSON.stringify(result) : null },
       where: { id: query.id },
     })
   }
