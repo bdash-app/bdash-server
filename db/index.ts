c74cec6ab72aff80a773883e5113f712d590c566
Kazuhito Hokamura <k.hokamura@gmail.com>
2021-06-12 19:51:42 +0900

    Allow password replacement by DATABASE_PASSWORD

--- db/index.ts
+++ db/index.ts
@@ -1,9 +1,19 @@
 import { enhancePrisma } from "blitz"
 import { PrismaClient } from "@prisma/client"
+import { format } from "util"
 
 const EnhancedPrisma = enhancePrisma(PrismaClient)
 
+const datasources = process.env.DATABASE_PASSWORD
+  ? {
+      db: {
+        url: format(process.env.DATABASE_URL, process.env.DATABASE_PASSWORD),
+      },
+    }
+  : undefined
+
 export * from "@prisma/client"
 export default new EnhancedPrisma({
+  datasources,
   log: ["query", "warn", "error"],
 })
